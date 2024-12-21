1cef63769be9d8a08548d10715d59012f873c29b
채희원/회원개발팀 <heewon.chae@musinsa.com>
2024-12-22 02:35:12 +0900

    add. spring boot 기본 파일 추가

---
+++ reservation-lecture/src/test/java/com/apply/course/ReservationLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apply.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
